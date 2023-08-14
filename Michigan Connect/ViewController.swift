b3509c8c7d50de5039d34e24811ae0a4e4a72133
pablosegovia <psegovia04@gmail.com>
2023-08-14 19:28:11 -0400

    Initial Commit

---
+++ Michigan Connect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Michigan Connect
+//
+//  Created by Pablo Segovia on 8/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
